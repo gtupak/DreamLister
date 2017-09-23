b748e9f60aa928399209e7c4a2f0bf024d5d0f65
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-23 10:17:59 -0400

    Implemented switch cases for the NSFetchResultsController and added a custom UITableViewCell.

---
+++ Dreamlister/View/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  Dreamlister
+//
+//  Created by Gabriel T on 2017-09-23.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
