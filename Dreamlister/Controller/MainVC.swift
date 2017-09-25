956a6582b035e0a061140f98ed61581351e8446d
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-25 10:37:47 -0400

    Now the user can add a new item to the list. Items get saved on the device using CoreData.

--- Dreamlister/Controller/MainVC.swift
+++ Dreamlister/Controller/MainVC.swift
@@ -22,7 +22,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         tableView.delegate = self
         tableView.dataSource = self
         
-        //generateTestData()
+//        generateTestData()
         attemptFetch()
     }
     
@@ -65,6 +65,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [dateSort]
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        controller.delegate = self
         
         self.controller = controller
         
